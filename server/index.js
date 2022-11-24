99a4e8e6b8d2a52d276a51e7657dfa821c3544af
Janmejay Buranpuri <jburanpu@uwo.ca>
2022-11-23 19:36:28 -0500

    lab3 content v2

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+const connectToDatabase = require("./db");
+const cors = require('cors');
+const { response } = require('express');
+const path = require('path');
+
+connectToDatabase();
+
+
+
+
+app.use(cors());
+app.use('/api/albums', require('./api/albums.js').router)
+app.use('/api/artists', require('./api/artists').router);
+app.use('/api/genres', require('./api/genres.js').router);
+app.use('/api/tracks', require('./api/tracks').router)
+app.use('/api/lists', require('./api/lists').router);
+app.use('./api/users', require('./api/users').router);
+
+
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('*', (req, res) => {
+    res.sendFile
+        (path.join(__dirname + '../client/index.html'));
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
