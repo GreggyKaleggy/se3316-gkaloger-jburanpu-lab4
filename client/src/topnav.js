efe8716e9290a275d596e62a5da49294199e0c9f
itsGergyy <60613210+GreggyKaleggy@users.noreply.github.com>
2022-11-28 20:30:20 -0500

    Introduced home page and working nav bar
    got the nav bar working in react, added the home page. User can now navigate between Home and Artists

---
+++ client/src/topnav.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+export default function Topnav(){
+
+    return(
+        <nav className="topnav">
+            <ul>
+                <li>
+                <ActiveLink href="/Home">Home</ActiveLink>
+                <ActiveLink href="/">Genres</ActiveLink>
+                <ActiveLink href="/">Lists</ActiveLink>
+                <ActiveLink href="/Artists">Artists</ActiveLink>
+                <ActiveLink href="/">Tracks</ActiveLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+function ActiveLink ({href, children, ...props }){
+    const path = window.location.pathname
+    return(
+            <a className = {path === href ? "active" : ""} href = {href} {...props}>{children}</a>
+    );
+}
