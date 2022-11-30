3d72c648ec7693767ef31a8b313874a869c2c2a0
Janmejay Buranpuri <jburanpu@uwo.ca>
2022-11-29 21:46:52 -0500

    added ability to deactivate users

--- server/api/users.js
+++ server/api/users.js
@@ -92,7 +92,41 @@ router.put('/changepassword', [
     }
 });
 
+//admin ability to make others admin
+router.put('/admin/changestatus', auth, async (req, res) => {
+    try {
+        const { email, isAdmin } = req.body;
+        let emailCheck = await User.findOne({ email: email });
+        if (!emailCheck) {
+            return res.status(400).json({ errors: [{ msg: 'Email address is not associated with any account' }] });
+        }
+        const user = await User.findOneAndUpdate({ email: email }, { $set: { isAdmin: isAdmin } }, { new: true });
+        res.json(user);
 
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//admin deactivating users
+router.put('/admin/deactivate', auth, async (req, res) => {
+    try {
+        const { email, deactivated } = req.body;
+        let emailCheck = await User.findOne({ email: email });
+        if (!emailCheck) {
+            return res.status(400).json({ errors: [{ msg: 'Email address is not associated with any account' }] });
+        }
+        const user = await User.findOneAndUpdate({ email: email }, { $set: { deactivated } }, { new: true });
+        res.json(user);
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+
+    }
+});
 
 module.exports = { router }
 
