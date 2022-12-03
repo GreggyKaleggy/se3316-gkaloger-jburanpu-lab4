5f1249fe4d9a64be935d0b475b2babffe49706be
Janmejay Buranpuri <jburanpu@uwo.ca>
2022-12-03 14:13:59 -0500

    debugging token for registering

--- client/src/register/register.js
+++ client/src/register/register.js
@@ -1,11 +1,10 @@
-import React, { useRef } from "react"
+import React, { useRef, useEffect } from "react"
 
-export default function Register () {
+export default function Register() {
     const usernameRef = useRef()
     const emailRef = useRef()
     const passwordRef = useRef()
-
-    async function registerUser (e){
+    async function registerUser(e) {
         const username = usernameRef.current.value
         const email = usernameRef.current.value
         const password = usernameRef.current.value
@@ -20,46 +19,54 @@ export default function Register () {
                 email: email,
                 password: password
             })
-        }).then(response => response.json())
-            .then(data => console.log(data))
+        }).then(response =>
+            response.json())
+            .then(data => {
+                if (data.errors) {
+                    console.log(data.errors)
+                    console.log(data.errors[0].msg)
+                } else {
+                    localStorage.setItem("x-auth-token", data.token);
+                    localStorage.setItem("isLoggedIn", true);
+                    window.location = "/"
+                }
+            }
+            )
     }
-
     return (
         <div >
-            <form >
-                <div >
-                    <h3>Sign Up</h3>
-                    <div>
-                        <label>Username</label>
-                        <input ref = {usernameRef}
-                            type="username"
-                            placeholder="Username"
-                        />
-                    </div>
-                    <div>
-                        <label>Email address</label>
-                        <input ref = {emailRef}
-                            type="email"
-                            placeholder="Email Address"
-                        />
-                    </div>
-                    <div>
-                        <label>Password</label>
-                        <input ref = {passwordRef}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div>
-                        <button type="submit" onClick = {registerUser}>
-                            Submit
-                        </button>
-                    </div>
-                    <p>
-                        Already have an account? <a href="/login">Log in</a>
-                    </p>
+            <div >
+                <h3>Sign Up</h3>
+                <div>
+                    <label>Username</label>
+                    <input ref={usernameRef}
+                        type="username"
+                        placeholder="Username"
+                    />
                 </div>
-            </form>
+                <div>
+                    <label>Email address</label>
+                    <input ref={emailRef}
+                        type="email"
+                        placeholder="Email Address"
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </div>
+                <div>
+                    <button onClick={registerUser}>
+                        Submit
+                    </button>
+                </div>
+                <p>
+                    Already have an account? <a href="/login">Log in</a>
+                </p>
+            </div>
         </div>
     )
 }
