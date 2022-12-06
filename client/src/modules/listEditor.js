907ae1f7fb5add3e88e4db22e167a1540ce250e4
itsGergyy <60613210+GreggyKaleggy@users.noreply.github.com>
2022-12-05 20:27:43 -0500

    List editor work
    Pulling values from the lists and populating inputs for editing

--- client/src/modules/listEditor.js
+++ client/src/modules/listEditor.js
@@ -1,7 +1,23 @@
-import React from "react";
-
+import React, { useState, useRef } from 'react';
 
 export default function ListEditor ({list}){
+    const [editState, setEditState] = useState(false)
+
+    const listNameRef = useRef()
+    const listDescriptionRef = useRef()
+    const tracksListRef = useRef()
+
+    var trackIDs = ""
+    list.tracklist.forEach(t => trackIDs = trackIDs + String(t.track_id) + " ")
+
+    async function ToggleEdit(e){
+        if (!editState){
+            setEditState(true)
+        } else{
+            setEditState(false)
+        }
+    }
+
     return(
         <>
         <div className="EditBox">
@@ -11,6 +27,23 @@ export default function ListEditor ({list}){
             <div>
                 Number of Tracks: {list.numberofTracks}
             </div>
+            <button onClick = {ToggleEdit}>Edit List</button>
+            {editState ? 
+            <>
+            <br/>
+            <label for="listName">List Name: </label>
+            <input ref={listNameRef} id = "listName" type="text" placeholder="List Name" value={list.name}/>
+            <br/>
+            <label for="listDesc">List Description: </label>
+            <input ref={listDescriptionRef} id = "listDesc" type="text" placeholder="Description (Optional)" value = {list.desc}/>
+            <br/>
+            <label for="trackIDs">Track IDs: </label>
+            <input ref={tracksListRef} id = "trackIDs" type="text" placeholder="Tracks" value = {trackIDs}/>
+            <br/>
+            <input type="button" defaultValue="Submit"/>
+            </> 
+            : null}
+
         </div>
         <br/>
         </>
