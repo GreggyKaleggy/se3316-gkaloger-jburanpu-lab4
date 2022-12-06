5575b33eb7970a4315e15dc2371d3e3f03133eea
itsGergyy <60613210+GreggyKaleggy@users.noreply.github.com>
2022-12-06 01:57:32 -0500

    Grant admin privileges hookup
    Hooked up granting admin privileges to a user

--- server/api/admins.js
+++ server/api/admins.js
@@ -9,11 +9,19 @@ const List = require('../schema/listSchema');
 
 
 //admin ability to make others admin
-router.put('/changestatus', auth, async (req, res) => {
+router.put('/changestatus', [
+    check('email', 'Please include valid email').isEmail().normalizeEmail()
+], auth, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        var errorMsg = errors.array().map(error => error.msg);
+        return res.status(400).json({ errors: [{ msg: errorMsg }] });
+    }
     try {
+
         const currentUser = await userSchema.findById(req.user.id);
         if (!currentUser.isAdmin) {
-            return res.status(401).json({ msg: 'You are not authorized to perform this action' });
+                return res.status(401).json({ errors: [{msg: 'You are not authorized to perform this action'}] });
         }
         const { email, isAdmin } = req.body;
         let emailCheck = await User.findOne({ email: email });
