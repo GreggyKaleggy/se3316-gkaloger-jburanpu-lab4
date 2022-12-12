f2174ddd2eb1ed499c0363406a98cf5ba080cc8b
Janmejay Buranpuri <jburanpu@uwo.ca>
2022-12-12 13:12:04 -0500

    Added react build to server, added path to combine

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const connectToDatabase = require('./db');
+const path = require('path');
 
 connectToDatabase();
-
 
 //all the routes
 app.use(cors());
@@ -16,6 +16,12 @@ app.use('/api/lists', require('./api/lists').router);
 app.use('/api/users', require('./api/users').router);
 app.use('/api/admins', require('./api/admins').router);
 app.use('/api/docs', require('./api/docs').router);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //port to listen on
 const port = process.env.PORT || 3001
